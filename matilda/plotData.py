c481a9102af53a4ea3dd6fbe9b6439c16703c8cd
Jan Ilavsky <jilavsky@gmail.com>
2025-07-10 14:24:39 -0500

    tweak range

--- matilda/plotData.py
+++ matilda/plotData.py
@@ -58,8 +58,8 @@ def plotUSAXSResults(ListOfresults, imagePath, isFlyscan=True):
     xlim = plt.xlim()   #returns a tuple of the form (xmin, xmax)
     ylim = plt.ylim()   #returns a tuple of the form (ymin, ymax)
     # Calculate the new limits, we want to keep tha max and limit the min to larger of existing value and max/1e10
-    new_xlim = (max(xlim[0], xlim[1] / 10**10), xlim[1])
-    new_ylim = (max(ylim[0], ylim[1] / 10**10), ylim[1])
+    new_xlim = (max(xlim[0], xlim[1] / 10**12), xlim[1])
+    new_ylim = (max(ylim[0], ylim[1] / 10**12), ylim[1])
     # Set the new limits
     plt.xlim(new_xlim)
     plt.ylim(new_ylim)
@@ -100,8 +100,8 @@ def plotUSAXSResults(ListOfresults, imagePath, isFlyscan=True):
     xlim = plt.xlim()   #returns a tuple of the form (xmin, xmax)
     ylim = plt.ylim()   #returns a tuple of the form (ymin, ymax)
     # Calculate the new limits, we want to keep tha max and limit the min to larger of existing value and max/1e10
-    new_xlim = (max(xlim[0], xlim[1] / 10**10), xlim[1])
-    new_ylim = (max(ylim[0], ylim[1] / 10**10), ylim[1])
+    new_xlim = (max(xlim[0], xlim[1] / 10**14), xlim[1])
+    new_ylim = (max(ylim[0], ylim[1] / 10**14), ylim[1])
     # Set the new limits
     plt.xlim(new_xlim)
     plt.ylim(new_ylim)
